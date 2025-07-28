57883b71ee38b864219aeb4f15ee27b5414b343e
kuro48 <kuroryu.basket@gmail.com>
2025-07-28 17:18:12 +0900

    ViewModelの作成

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineViewModel.kt
@@ -0,0 +1,27 @@
+package com.dmm.bootcamp.yatter2025.ui.timeline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dmm.bootcamp.yatter2025.domain.repository.YweetRepository
+import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.converter.YweetConverter
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class PublicTimelineViewModel(
+    private val yweetRepository: YweetRepository,
+    ) : ViewModel() {
+    private val _uiState: MutableStateFlow<PublicTimelineUiState> = MutableStateFlow(PublicTimelineUiState.empty())
+    val uiState: StateFlow<PublicTimelineUiState> = _uiState.asStateFlow()
+
+    private suspend fun fetchPublicTimeline() {
+        val yweetList = yweetRepository.findAllPublic()
+        _uiState.update {
+            it.copy(
+                yweetList = YweetConverter.convertToBindingModel(yweetList),
+            )
+        }
+    }
+}
