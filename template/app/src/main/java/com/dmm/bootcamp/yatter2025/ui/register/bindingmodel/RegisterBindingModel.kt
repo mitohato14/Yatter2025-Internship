6788862c9ec4bade8f5ccef2d669f4722c8614de
kuro48 <kuroryu.basket@gmail.com>
2025-07-30 12:45:22 +0900

    RegisterBindingModelの実装

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/register/bindingmodel/RegisterBindingModel.kt
@@ -0,0 +1,12 @@
+package com.dmm.bootcamp.yatter2025.ui.register.bindingmodel
+
+import java.net.URL
+
+data class RegisterBindingModel(
+    val username: String,
+    val password: String,
+    val displayName: String,
+    val avatar: URL?,
+    val header: URL?,
+    val note: String,
+)
