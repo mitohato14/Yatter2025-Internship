61646545d98ee3c152935b2f353765ea1da84d51
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 12:34:53 +0900

    ViewModelの作成

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailViewModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailViewModel.kt
@@ -1,4 +1,53 @@
 package com.dmm.bootcamp.yatter2025.ui.timeline
 
-class PostDetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dmm.bootcamp.yatter2025.common.navigation.Destination
+import com.dmm.bootcamp.yatter2025.common.navigation.PopBackDestination
+import com.dmm.bootcamp.yatter2025.domain.model.YweetId
+import com.dmm.bootcamp.yatter2025.domain.repository.YweetRepository
+import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.converter.YweetConverter
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class PostDetailViewModel(
+    private val yweetRepository: YweetRepository
+) : ViewModel(){
+    private val _uiState: MutableStateFlow<PostDetailUiState> = MutableStateFlow(PostDetailUiState.empty())
+    val uiState: StateFlow<PostDetailUiState> = _uiState.asStateFlow()
+
+    private val _destination = MutableStateFlow<Destination?>(null)
+    val destination: StateFlow<Destination?> = _destination.asStateFlow()
+
+    private suspend fun fetchPostDetail(yweetId: String){
+        val yweet = yweetRepository.findById(YweetId(yweetId))
+
+        if (yweet != null) {
+            _uiState.update {
+                it.copy(
+                    yweet = YweetConverter.convertToBindingModel(yweet)
+                )
+            }
+        }
+    }
+
+    fun onResume(yweetId: String) {
+        viewModelScope.launch {
+            _uiState.update { it.copy( isLoading = true ) }
+            fetchPostDetail(yweetId)
+            _uiState.update { it.copy( isLoading = false )  }
+        }
+    }
+
+    fun onClickNavIcon() {
+        _destination.value = PopBackDestination
+    }
+
+    fun onCompleteNavigation() {
+        _destination.value = null
+    }
+
 }
