1f5098db178214b98c5d6ada466ef2cfc4e403e7
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 16:37:50 +0900

    BindingModelの修正

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/UserBindingModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/UserBindingModel.kt
@@ -2,7 +2,7 @@ package com.dmm.bootcamp.yatter2025.ui.profile.bindinmodel
 
 data class UserBindingModel(
     val username: String,
-    val displayName: String,
+    val displayName: String?,
     val note: String?,
     val avatar: String?,
     val header: String?,
---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/UserConverter.kt
@@ -0,0 +1,4 @@
+package com.dmm.bootcamp.yatter2025.ui.profile.bindinmodel
+
+object UserConverter {
+}
