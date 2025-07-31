ce1e00394d78faaedb88a5fcae5108403f73e0d8
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 12:28:02 +0900

    Destinationの作成

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailDestination.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailDestination.kt
@@ -1,4 +1,18 @@
 package com.dmm.bootcamp.yatter2025.ui.timeline
 
-class PostDetailDestination {
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.dmm.bootcamp.yatter2025.common.navigation.Destination
+import okhttp3.Route
+
+class PostDetailDestination : Destination(ROUTE) {
+    companion object {
+        private const val ROUTE = "postDetail"
+
+        fun createNode(builder: NavGraphBuilder) {
+            builder.composable(ROUTE) {
+                PostDetailPage()
+            }
+        }
+    }
 }
