f8c06be312d6f9260c96334cf81f4cd1cadef71f
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 12:01:56 +0900

    ファイルの作成

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailPage.kt
@@ -0,0 +1,11 @@
+package com.dmm.bootcamp.yatter2025.ui.timeline
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PostDetailPage(
+
+) {
+
+}
---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PostDetailUiState.kt
@@ -0,0 +1,6 @@
+package com.dmm.bootcamp.yatter2025.ui.timeline
+
+import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.ImageBindingModel
+import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.YweetBindingModel
+
+data class PostDetailUiState()
