f630bbb03b08daeea09a4d402b8a01b1f9b518ec
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 17:32:25 +0900

    プロフィール画面への導線実装

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/UserProfileDestination.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/UserProfileDestination.kt
@@ -1,4 +1,42 @@
 package com.dmm.bootcamp.yatter2025.ui.profile
 
-class UserProfileDestination {
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import coil.request.ImageRequest
+import com.dmm.bootcamp.yatter2025.common.navigation.Destination
+
+class UserProfileDestination(
+    private val username: String?
+) : Destination(ROUTE) {
+    override fun buildRoute(): String {
+        return buildString {
+            append(ROUTE_PATH)
+            if (username != null) {
+                append("$KEY_USERNAME=$username")
+            }
+        }
+    }
+
+    companion object {
+        const val ROUTE_PATH = "userProfile"
+        const val KEY_USERNAME = "username"
+        const val ROUTE = "$ROUTE_PATH?$KEY_USERNAME={$KEY_USERNAME}"
+
+        fun createNode(builder: NavGraphBuilder) {
+            builder.composable(
+                route = ROUTE,
+                arguments = listOf(
+                    navArgument(KEY_USERNAME) {
+                        defaultValue = ""
+                        type = NavType.StringType
+                    },
+                ),
+            ) { backStackEntry ->
+                val username = requireNotNull(backStackEntry.arguments?.getString(KEY_USERNAME))
+                UserProfilePage(username = username)
+            }
+        }
+    }
 }
--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/RelationshipBindingModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/RelationshipBindingModel.kt
@@ -1,3 +1,3 @@
 package com.dmm.bootcamp.yatter2025.ui.profile.bindinmodel
 
-data class RelationshipBindingModel()
+//data class RelationshipBindingModel()
