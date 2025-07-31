ab66d86cc8a2af7ff06bd1f6377843527de276ac
kuro48 <kuroryu.basket@gmail.com>
2025-07-31 15:55:04 +0900

    UserBindingModelの作成

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/UserBindingModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/UserBindingModel.kt
@@ -1,3 +1,11 @@
 package com.dmm.bootcamp.yatter2025.ui.profile.bindinmodel
 
-data class UserBindingModel()
+data class UserBindingModel(
+    val username: String,
+    val displayName: String,
+    val note: String?,
+    val avatar: String?,
+    val header: String?,
+    val followingCount: Int,
+    val followerCount:Int,
+)
